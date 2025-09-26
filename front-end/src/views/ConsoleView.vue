387644628406a0c5970791add94193eccf15bbe9
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2025-09-26 23:54:01 +0200

    implemented sidebar

--- front-end/src/views/ConsoleView.vue
+++ front-end/src/views/ConsoleView.vue
@@ -1,11 +1,38 @@
 <template>
-    this is the console
+    <div class="console-view">
+        <div class="side-bar">
+            <div class="logo">
+                <img src="/logo.png" alt="">
+            </div>
+            <div class="sections">
+                <div class="leading">
+                    <div class="section">
+                        <!-- <Icon icon="solar:home-2-linear" :height="35" /> -->
+                        <Icon icon="solar:home-2-bold" height="35" />
+                    </div>
+                    <div class="section">
+                        <Icon icon="solar:archive-minimalistic-linear" height="35" />
+                        <!-- <Icon icon="solar:archive-minimalistic-bold" height="35" /> -->
+                    </div>
+                </div>
+                <div class="bottom">
+                    <div class="section">
+                        <Icon icon="solar:logout-2-outline" height="35" />
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-view">
+            <router-view/>
+        </div>
+    </div>
 </template>
 
 <script setup>
 import { onMounted } from 'vue';
 import { useRouter } from 'vue-router';
 import { useAuthStore } from '@/stores/auth';
+import { Icon } from "@iconify/vue";
 
 const authStore = useAuthStore();
 const router = useRouter();
@@ -17,3 +44,59 @@ onMounted(() => {
   }
 })
 </script>
+
+<style scoped>
+.console-view {
+  display: flex;
+  height: 100vh;
+  width: 100vw;
+
+  .side-bar {
+    width: 80px;
+    background-color: #df0000;
+    color: white;
+
+    .logo {
+        width: 100%;
+        margin-bottom: 20px;
+        img {
+            padding-top: 5px;
+            width: 100%;
+            height: auto;
+        }
+    }
+
+    .sections {
+        display: flex;
+        flex-direction: column;
+        justify-content: space-between;
+        height: calc(100vh - 80px);
+
+        .leading, .bottom {
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            align-items: center;
+
+            .section {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+                padding: 10px;
+                border-radius: 10px;
+                transition: background-color 0.2s ease;
+
+                &:hover {
+                    cursor: pointer;
+                    background-color: #b30000;
+                }
+            }
+        }
+
+        .bottom .section {
+            margin-bottom: 10px;
+        }
+    }
+  }
+}
+</style>
