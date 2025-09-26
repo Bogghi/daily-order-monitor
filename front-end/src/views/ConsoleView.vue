5e5c794b69de924192cd94a239c59d59cc2f85c8
Matteo Simone Borghi <Bogghi@users.noreply.github.com>
2025-09-26 23:16:58 +0200

    implemented console login guarding

--- front-end/src/views/ConsoleView.vue
+++ front-end/src/views/ConsoleView.vue
@@ -3,4 +3,17 @@
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+import { useRouter } from 'vue-router';
+import { useAuthStore } from '@/stores/auth';
+
+const authStore = useAuthStore();
+const router = useRouter();
+
+onMounted(() => {
+  if(!authStore.isAuthenticated) {
+    authStore.logout();
+    router.push('/login');
+  }
+})
 </script>
